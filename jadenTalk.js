c058e32de1bbc776b07e8f47afd6d06c180d2763
Gábor Szekeres <mail.szekg@gmail.com>
2024-12-16 17:22:39 +0100

    finished jadenTalk

--- jadenTalk.js
+++ jadenTalk.js
@@ -1,5 +1,15 @@
 function jadenTalk(text) {
-  //write your code here
+  const unneededPunctuation = /[.,!?;:]/g;
+
+  let cleanText = text.replace(unneededPunctuation, "");
+
+  const words = cleanText.split(" ");
+
+  const capitalizedWords = words.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  });
+
+  return capitalizedWords.join(" ");
 }
 
 module.exports = jadenTalk
