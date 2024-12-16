0edf12175a495c79fba84f4138d3756ec4207546
Gábor Szekeres <mail.szekg@gmail.com>
2024-12-16 17:45:21 +0100

    added other two functions

--- carRenting.js
+++ carRenting.js
@@ -1,5 +1,15 @@
 function carRenting(cars, distance) {
-  //write your code here
+  const goodCars = cars.filter(car => {
+    //to find max distance => (fuel amount * 100 km) / average consumption per 100 km
+    const maxDistance = (car.fuelAmount * 100) / car.avgConsumption;
+    return maxDistance >= distance;
+  });
+
+  if (goodCars.length === 0) {
+    return null;
+  }
+
+  return goodCars.map(car => car.type);
 }
 
 module.exports = carRenting
